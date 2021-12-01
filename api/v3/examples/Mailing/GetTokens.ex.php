724a51c12dfb0a1d547ff2fc16e44f5d45ce265f
scardinius <scardinius@chords.pl>
2021-12-01 14:21:07 +0100

    (patch.base-url-token-5.49.4) Add token baseUrl to domain with trailing slash (working in mailings)

--- api/v3/examples/Mailing/GetTokens.ex.php
+++ api/v3/examples/Mailing/GetTokens.ex.php
@@ -74,6 +74,7 @@ function mailing_gettokens_expectedresult() {
       '{domain.description}' => 'Domain Description',
       '{domain.now}' => 'Current time/date',
       '{domain.tax_term}' => 'Sales tax term (e.g VAT)',
+      '{domain.baseUrl}' => 'Domain absolute base url with trailing slash',
       '{contact.checksum}' => 'Checksum',
       '{contact.current_employer}' => 'Current Employer',
       '{contact.world_region}' => 'World Region',
