1076c50a535cb730fc8943f68cfceb66fecd8306
Artur <kontakt@iprosoft.pl>
2024-06-22 12:03:46 +0200

    Add tplParams to mail content for alterMailContent hook

--- CRM/Core/DomainTokens.php
+++ CRM/Core/DomainTokens.php
@@ -57,6 +57,7 @@ class CRM_Core_DomainTokens extends AbstractTokenSubscriber {
       'description' => ts('Domain Description'),
       'now' => ts('Current time/date'),
       'base_url' => ts('Domain absolute base url'),
+      'baseUrl' => ts('Domain absolute base url with trailing slash'),
       'tax_term' => ts('Sales tax term (e.g VAT)'),
       'empowered_by_civicrm_image_url' => ts('Empowered By CiviCRM Image'),
     ];
@@ -129,6 +130,7 @@ class CRM_Core_DomainTokens extends AbstractTokenSubscriber {
       $tokens['phone'] = $phone['phone'] ?? '';
       $tokens['email'] = $email['email'] ?? '';
       $tokens['base_url'] = Civi::paths()->getVariable('cms.root', 'url');
+      $tokens['baseUrl'] = (new CRM_Utils_System_Drupal())->getAbsoluteBaseURL();
       $tokens['empowered_by_civicrm_image_url'] = CRM_Core_Config::singleton()->userFrameworkResourceURL . 'i/civi99.png';
       $tokens['tax_term'] = (string) Civi::settings()->get('tax_term');
       Civi::cache('metadata')->set($cacheKey, $tokens);
